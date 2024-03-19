d3b86974c23642e9531b261e39d3786c85e66328
Andrey-Fedosov <andrefedosov@gmail.com>
2024-03-19 21:31:57 +0300

    feature: add feature flag implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { FeatureFlagGlobalState } from "./components/feature-flag/context/FeatureFlagGlobalState";
+import { FeatureFlags } from "./components/feature-flag/FeatureFlags";
 
 function App() {
   return (
     <div className="App">
-      <h2>FeatureFlag project</h2>
+      <FeatureFlagGlobalState>
+        <FeatureFlags />
+      </FeatureFlagGlobalState>
     </div>
   );
 }
